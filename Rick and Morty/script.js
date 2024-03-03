099672ee0409015ee0bdb499ad27f275adb0c4d7
Max Reis <maxreisdossantos@hotmail.com>
2024-03-03 14:43:16 -0300

    innicial commit

--- Rick and Morty/script.js
+++ Rick and Morty/script.js
@@ -1,7 +1,7 @@
 const url = "https://rickandmortyapi.com/api/character"
 
 const characterId = document.querySelector("#characterId")
-const btn = document.querySelector("#btn")
+const btnSearch = document.querySelector("#btn-search")
 const content = document.querySelector(".content")
 const img = document.querySelector("#img")
 
@@ -11,35 +11,43 @@ async function fetchApi(id) {
     return data
 }
 
-btn.addEventListener("click", async (event) => {
+btnSearch.addEventListener("click", async (event) => {
     event.preventDefault();
-
     const name = document.createElement("h2")
     const status = document.createElement("p")
     const img = document.createElement("img")
 
-
+   
     if (characterId.value > 0 && characterId.value <= 550) {
         event.preventDefault();
         const res = await fetchApi(characterId.value)
         img.src = res.image
+        img.style.width = "50%"
         name.textContent = `${res.name}`;
         status.textContent = `Status: ${res.status}`;
 
         content.innerHTML = "";
-
         content.appendChild(name)
         content.appendChild(status)
         content.appendChild(img)
 
+    } else if (characterId.value === "") {
+        window.alert("Digite um número de 1 a 550")
     } else{
-        
         const error = document.createElement("h2")
         error.textContent = "Personagem Inexistente"
         content.innerHTML = "";
         content.appendChild(error)
     }
+    
 
 })
 
 fetchApi()
+
+// on window load
+window.onload = () => {
+   characterId.value = 53
+   btnSearch.click()
+
+}
